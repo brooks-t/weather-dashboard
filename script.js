67cbdfda82c3362397747b7ef153e5a53c048a68
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-03 13:50:09 -0700

    connected search button up to current weather api. worked on bootstrap styling layout. added header to results section

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
 today = moment().format('L');
-var searchField = document.querySelector('#search-field');
 var searchButton = document.querySelector('#search-button');
+var searchField = document.querySelector('#search-field');
 
-currentWeatherCall = 'http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
+var uvCall = 'http://api.openweathermap.org/data/2.5/uvi?lat=47.6062&lon=-122.3321&appid=8b9474b76db97cf9c54177ce617e7e88';
 
-uvCall = 'http://api.openweathermap.org/data/2.5/uvi?lat=47.6062&lon=-122.3321&appid=8b9474b76db97cf9c54177ce617e7e88';
+var forecastCall = 'http://api.openweathermap.org/data/2.5/forecast?q=' + searchField.value + '&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
 
-forecastCall = 'http://api.openweathermap.org/data/2.5/forecast?q=Seattle&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
-
-
-
-function getWeather() {
-    fetch(currentWeatherCall)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        var cityLat = data.coord.lat;
-        var cityLon = data.coord.lon;
-        console.log('CURRENT WEATHER \n-------------');
-        console.log(data);
-        console.log(data.name);
-        console.log(today);
-        console.log(data.main.temp);
-        console.log(data.main.humidity);
-        console.log(data.wind.speed);
-        console.log(data.weather[0].description);
-        console.log(cityLat);
-        console.log(cityLon);
-  })
-}
 
 // not sure where the UV index is listed in the data returned here. there is a value key but not sure if that's it. the documentation says its a float longitude for returned data which doesn't make any sense because they give that to us with the 'lon' key.
 function getUV() {
@@ -51,6 +27,7 @@ function getForecast() {
     .then(function (data) {
         tomorrow = moment().add(1, 'd').format('L');
         console.log('FORECAST \n-------------');
+        console.log(forecastCall);
         console.log(data);
         console.log(tomorrow);
         console.log(data.list[8].weather[0].description);
@@ -59,16 +36,38 @@ function getForecast() {
     })
 }
 
+searchButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    var citySearch = searchField; 
+    console.log("Searched for: " + citySearch.value);
+
+    var currentWeatherCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + citySearch.value + '&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
+
+    fetch(currentWeatherCall)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var cityLat = data.coord.lat;
+            var cityLon = data.coord.lon;
+            console.log('CURRENT WEATHER \n-------------');
+            console.log(data);
+            console.log(data.name);
+            console.log(today);
+            console.log(data.main.temp);
+            console.log(data.main.humidity);
+            console.log(data.wind.speed);
+            console.log(data.weather[0].description);
+            console.log(cityLat);
+            console.log(cityLon);
+        })
+})
+
+
 //getUV();
 
 
 // TODO: finish building this event listener for search button
-searchButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log("search button clicked!");
-    getWeather();
-    getForecast();
-})
 
 
 /*for(i = 0; i < data.length; i++) {

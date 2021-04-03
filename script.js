73ae14db46370dff0ad29c37d17f995044f44a33
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-03 09:25:06 -0700

    added uv index api call. works but more testing to be done

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
-today = moment().calendar();
+today = moment().format('L');
 
-requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=8b9474b76db97cf9c54177ce617e7e88';
+currentWeatherCall = 'http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
+
+uvCall = 'http://api.openweathermap.org/data/2.5/uvi?lat=47.6062&lon=-122.3321&appid=8b9474b76db97cf9c54177ce617e7e88';
+
+forecastCall = 'api.openweathermap.org/data/2.5/forecast?q=Seattle&appid=8b9474b76db97cf9c54177ce617e7e88';
+
+
 
 function getApi() {
-    fetch(requestUrl)
+    fetch(currentWeatherCall)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var cityLat = data.coord.lat;
+        var cityLon = data.coord.lon;
+        console.log('Fetch Response \n-------------');
+        console.log(data);
+        console.log(data.name);
+        console.log(today);
+        console.log(data.main.temp);
+        console.log(data.main.humidity);
+        console.log(data.wind.speed);
+        console.log(data.weather[0].description);
+        console.log(cityLat);
+        console.log(cityLon);
+  })
+}
+
+// not sure where the UV index is listed in the data returned here. there is a value key but not sure if that's it. the documentation says its a float longitude for returned data which doesn't make any sense because they give that to us with the 'lon' key.
+function getUV() {
+    fetch(uvCall)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log('Fetch Response \n-------------');
     console.log(data);
-    console.log(data.name);
-    console.log(data.weather[0].main);
   })
 }
 
+function getForecast() {
+    fetch(forecastCall)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Fetch Response \n-------------');
+    console.log(data);
+  })
+}
 
 getApi();
+
+getUV();
 
 
 /*for(i = 0; i < data.length; i++) {

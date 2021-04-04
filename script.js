3f167dab3931fbddac0ca54382f75779f0896492
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-04 15:02:31 -0700

    got weather icons to populate for current weather and forecast. added styling for sizing and position

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var humidResult = document.querySelector('#humid-result');
 var windResult = document.querySelector('#wind-result');
 var uvResult = document.querySelector('#uv-result');
 var pastSearch = document.querySelector('#past-search');
+var currentIcon = document.querySelector('#current-icon').src;
+var weatherIcon = document.getElementById('#current-icon');
 var searchCity;
 var searchAgain;
 
@@ -52,8 +54,6 @@ function searchWeather(event) {
     savedCity.textContent = searchField.value;
     pastSearch.appendChild(savedCity);
 
-    headResult.textContent = searchCity + ' ' + '(' + today + ')' + '⛅';
-
     var currentWeatherCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
 
     fetch(currentWeatherCall)
@@ -63,15 +63,20 @@ function searchWeather(event) {
         .then(function (data) {
             var cityLat = data.coord.lat;
             var cityLon = data.coord.lon;
+            var cityIcon = data.weather[0].icon;
             var cityTemp = data.main.temp;
             var cityHumid = data.main.humidity;
             var cityWind = data.wind.speed;
             var cityUv = 'TBD';
 
+            headResult.textContent = searchCity + ' ' + '(' + today + ')';
+            currentIcon = 'http://openweathermap.org/img/wn/' + cityIcon + '@2x.png';
+            document.getElementById('current-icon').src = currentIcon;
             tempResult.textContent = 'Temperature: ' + cityTemp;
             humidResult.textContent = 'Humidity: ' + cityHumid + '%';
             windResult.textContent = 'Wind Speed: ' + cityWind + 'MPH';
             uvResult.textContent = 'UV Index: Unknown';
+            console.log(data);
         })
 
         var forecastCall = 'http://api.openweathermap.org/data/2.5/forecast?q=' + searchCity + '&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
@@ -83,21 +88,27 @@ function searchWeather(event) {
         .then(function (data) {
             // Day 1
             var cardHeader1 = document.querySelector('#card-header-1');
-            var cardIcon1 = document.querySelector('#card-icon-1');
+            var cardIcon1 = document.querySelector('#card-icon-1').src;
             var cardTemp1 = document.querySelector('#card-temp-1');
             var cardHumid1 = document.querySelector('#card-humid-1');
 
             cardHeader1.textContent = moment().add(1, 'd').format('L');
+            foreIcon1 = data.list[7].weather[0].icon;
+            cardIcon1 = 'http://openweathermap.org/img/wn/' + foreIcon1 + '@2x.png';
+            document.getElementById('card-icon-1').src =  cardIcon1;
             cardTemp1.textContent = 'Temp: ' + data.list[7].main.temp + ' F';
             cardHumid1.textContent = 'Humidity: ' + data.list[7].main.humidity + '%';
 
             // Day 2
             var cardHeader2 = document.querySelector('#card-header-2');
-            var cardIcon2 = document.querySelector('#card-icon-2');
+            var cardIcon2 = document.querySelector('#card-icon-2').src;
             var cardTemp2 = document.querySelector('#card-temp-2');
             var cardHumid2 = document.querySelector('#card-humid-2');
 
             cardHeader2.textContent = moment().add(2, 'd').format('L');
+            foreIcon2 = data.list[15].weather[0].icon;
+            cardIcon2 = 'http://openweathermap.org/img/wn/' + foreIcon2 + '@2x.png';
+            document.getElementById('card-icon-2').src =  cardIcon2;
             cardTemp2.textContent = 'Temp: ' + data.list[15].main.temp + ' F';
             cardHumid2.textContent = 'Humidity: ' + data.list[15].main.humidity + '%';
 
@@ -108,6 +119,9 @@ function searchWeather(event) {
             var cardHumid3 = document.querySelector('#card-humid-3');
 
             cardHeader3.textContent = moment().add(3, 'd').format('L');
+            foreIcon3 = data.list[23].weather[0].icon;
+            cardIcon3 = 'http://openweathermap.org/img/wn/' + foreIcon3 + '@2x.png';
+            document.getElementById('card-icon-3').src =  cardIcon3;
             cardTemp3.textContent = 'Temp: ' + data.list[23].main.temp + ' F';
             cardHumid3.textContent = 'Humidity: ' + data.list[23].main.humidity + '%';
 
@@ -118,6 +132,9 @@ function searchWeather(event) {
             var cardHumid4 = document.querySelector('#card-humid-4');
 
             cardHeader4.textContent = moment().add(4, 'd').format('L');
+            foreIcon4 = data.list[31].weather[0].icon;
+            cardIcon4 = 'http://openweathermap.org/img/wn/' + foreIcon4 + '@2x.png';
+            document.getElementById('card-icon-4').src =  cardIcon4;
             cardTemp4.textContent = 'Temp: ' + data.list[31].main.temp + ' F';
             cardHumid4.textContent = 'Humidity: ' + data.list[31].main.humidity + '%';
 
@@ -128,6 +145,9 @@ function searchWeather(event) {
             var cardHumid5 = document.querySelector('#card-humid-5');
 
             cardHeader5.textContent = moment().add(5, 'd').format('L');
+            foreIcon5 = data.list[39].weather[0].icon;
+            cardIcon5 = 'http://openweathermap.org/img/wn/' + foreIcon5 + '@2x.png';
+            document.getElementById('card-icon-5').src =  cardIcon5;
             cardTemp5.textContent = 'Temp: ' + data.list[39].main.temp + ' F';
             cardHumid5.textContent = 'Humidity: ' + data.list[39].main.humidity + '%';
         })
@@ -142,8 +162,6 @@ function searchHistory(event) {
         console.log("You searched for " + searchAgain + " again!");
     }
 
-    headResult.textContent = searchAgain + ' ' + '(' + today + ')' + '⛅';
-
     var currentWeatherCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + searchAgain + '&units=imperial&appid=8b9474b76db97cf9c54177ce617e7e88';
 
     fetch(currentWeatherCall)
@@ -153,11 +171,15 @@ function searchHistory(event) {
         .then(function (data) {
             var cityLat = data.coord.lat;
             var cityLon = data.coord.lon;
+            var cityIcon = data.weather[0].icon;
             var cityTemp = data.main.temp;
             var cityHumid = data.main.humidity;
             var cityWind = data.wind.speed;
             var cityUv = 'TBD';
 
+            headResult.textContent = searchCity + ' ' + '(' + today + ')';
+            currentIcon = 'http://openweathermap.org/img/wn/' + cityIcon + '@2x.png';
+            document.getElementById('current-icon').src = currentIcon;
             tempResult.textContent = 'Temperature: ' + cityTemp;
             humidResult.textContent = 'Humidity: ' + cityHumid + '%';
             windResult.textContent = 'Wind Speed: ' + cityWind + 'MPH';
@@ -173,21 +195,27 @@ function searchHistory(event) {
         .then(function (data) {
             // Day 1
             var cardHeader1 = document.querySelector('#card-header-1');
-            var cardIcon1 = document.querySelector('#card-icon-1');
+            var cardIcon1 = document.querySelector('#card-icon-1').src;
             var cardTemp1 = document.querySelector('#card-temp-1');
             var cardHumid1 = document.querySelector('#card-humid-1');
 
             cardHeader1.textContent = moment().add(1, 'd').format('L');
+            foreIcon1 = data.list[7].weather[0].icon;
+            cardIcon1 = 'http://openweathermap.org/img/wn/' + foreIcon1 + '@2x.png';
+            document.getElementById('card-icon-1').src =  cardIcon1;
             cardTemp1.textContent = 'Temp: ' + data.list[7].main.temp + ' F';
             cardHumid1.textContent = 'Humidity: ' + data.list[7].main.humidity + '%';
 
             // Day 2
             var cardHeader2 = document.querySelector('#card-header-2');
-            var cardIcon2 = document.querySelector('#card-icon-2');
+            var cardIcon2 = document.querySelector('#card-icon-2').src;
             var cardTemp2 = document.querySelector('#card-temp-2');
             var cardHumid2 = document.querySelector('#card-humid-2');
 
             cardHeader2.textContent = moment().add(2, 'd').format('L');
+            foreIcon2 = data.list[15].weather[0].icon;
+            cardIcon2 = 'http://openweathermap.org/img/wn/' + foreIcon2 + '@2x.png';
+            document.getElementById('card-icon-2').src =  cardIcon2;
             cardTemp2.textContent = 'Temp: ' + data.list[15].main.temp + ' F';
             cardHumid2.textContent = 'Humidity: ' + data.list[15].main.humidity + '%';
 
@@ -198,6 +226,9 @@ function searchHistory(event) {
             var cardHumid3 = document.querySelector('#card-humid-3');
 
             cardHeader3.textContent = moment().add(3, 'd').format('L');
+            foreIcon3 = data.list[23].weather[0].icon;
+            cardIcon3 = 'http://openweathermap.org/img/wn/' + foreIcon3 + '@2x.png';
+            document.getElementById('card-icon-3').src =  cardIcon3;
             cardTemp3.textContent = 'Temp: ' + data.list[23].main.temp + ' F';
             cardHumid3.textContent = 'Humidity: ' + data.list[23].main.humidity + '%';
 
@@ -208,6 +239,9 @@ function searchHistory(event) {
             var cardHumid4 = document.querySelector('#card-humid-4');
 
             cardHeader4.textContent = moment().add(4, 'd').format('L');
+            foreIcon4 = data.list[31].weather[0].icon;
+            cardIcon4 = 'http://openweathermap.org/img/wn/' + foreIcon4 + '@2x.png';
+            document.getElementById('card-icon-4').src =  cardIcon4;
             cardTemp4.textContent = 'Temp: ' + data.list[31].main.temp + ' F';
             cardHumid4.textContent = 'Humidity: ' + data.list[31].main.humidity + '%';
 
@@ -218,6 +252,9 @@ function searchHistory(event) {
             var cardHumid5 = document.querySelector('#card-humid-5');
 
             cardHeader5.textContent = moment().add(5, 'd').format('L');
+            foreIcon5 = data.list[39].weather[0].icon;
+            cardIcon5 = 'http://openweathermap.org/img/wn/' + foreIcon5 + '@2x.png';
+            document.getElementById('card-icon-5').src =  cardIcon5;
             cardTemp5.textContent = 'Temp: ' + data.list[39].main.temp + ' F';
             cardHumid5.textContent = 'Humidity: ' + data.list[39].main.humidity + '%';
         })
